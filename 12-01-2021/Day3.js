3ba6fffb18f347f88c984730e1799a92407ffad1
Khodidas Gamdha <kagamdha@gmail.com>
2021-01-12 19:01:12 +0530

    loop, hoisting and function

--- 12-01-2021/Day3.js
+++ 12-01-2021/Day3.js
@@ -1,47 +1,47 @@
-// // In JavaScript a Variable can be used before it has been declared
+// In JavaScript a Variable can be used before it has been declared
 
-// console.log(counter);
-// var counter = 1; // undefined
+console.log(counter);
+var counter = 1; // undefined
 
-// var abc;
-// console.log(abc); // undefined
-// abc = 1;
+var abc;
+console.log(abc); // undefined
+abc = 1;
 
-// console.log(abcd); // ReferenceError: Cannot access 'counter' before initialization
-// let abcd = 1;
+console.log(abcd); // ReferenceError: Cannot access 'counter' before initialization
+let abcd = 1;
 
-// // function declaration
-// let x = 20,
-//     y = 10;
+// function declaration
+let x = 20,
+    y = 10;
 
-// let result = add(x,y);
-// console.log(result); // 30
+let result = add(x,y);
+console.log(result); // 30
 
-// function add(a, b){
-// return a + b;
-// }
+function add(a, b){
+return a + b;
+}
 
-// // function expression
-// let x = 20,
-//     y = 10;
+// function expression
+let x = 20,
+    y = 10;
 
-// let result = add(x,y);
-// console.log(result); // add not defined
+let result = add(x,y);
+console.log(result); // add not defined
 
-// var add = function() {
-// return x + y;
-// }
+var add = function() {
+return x + y;
+}
 
-// // function expression
-// let x = 20,
-//     y = 10;
+// function expression
+let x = 20,
+    y = 10;
 
-// var add = function() {
-// return x + y;
-// }
+var add = function() {
+return x + y;
+}
 
-// let result = add(x,y);
-// console.log(result); // 30
+let result = add(x,y);
+console.log(result); // 30
 
 // this keyword
 var john = {
