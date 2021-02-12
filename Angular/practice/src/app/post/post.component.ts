42e698f50b7253ed3be850ec40114230d534b6ce
Khodidas Gamdha <kagamdha@gmail.com>
2021-02-12 11:27:08 +0530

    Http post crud operations

--- Angular/practice/src/app/post/post.component.ts
+++ Angular/practice/src/app/post/post.component.ts
@@ -4,27 +4,43 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent {
-
   posts: any[];
   private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: HttpClient) { 
-    http.get(this.url).subscribe(response => this.posts = response as []);
+  // geting data from site and put in array
+  constructor(private http: HttpClient) {
+    http.get(this.url).subscribe((response) => (this.posts = response as []));
   }
 
+  // create new post  
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
 
-    this.http.post(this.url, JSON.stringify(post))
-    .subscribe(response => { 
-      post['id'] = (response as [])['id'], 
-      this.posts.splice(0, 0, post)
+    this.http.post(this.url, JSON.stringify(post)).subscribe((response) => {
+      (post['id'] = (response as [])['id']), this.posts.splice(0, 0, post);
     });
-  
+  }
+
+  // update post
+  updatePost(post) {
+    //   this.http.post(this.url, JSON.stringfy(post))
+    this.http
+      .patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  // delete post
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id).subscribe((responce) => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
   }
 
 }
