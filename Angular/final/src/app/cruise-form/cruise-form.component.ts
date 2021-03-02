3d725bd3eed7498ef30ca989bcbaa454569a6be6
Khodidas Gamdha <kagamdha@gmail.com>
2021-03-02 16:36:30 +0530

    Form

--- Angular/final/src/app/cruise-form/cruise-form.component.ts
+++ Angular/final/src/app/cruise-form/cruise-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cruise-form',
@@ -9,33 +9,27 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 export class CruiseFormComponent implements OnInit {
 
   form;
-  curruntDate = new Date();
   
   constructor(private fb: FormBuilder) { }
   
   ngOnInit(): void {
     this.form = this.fb.group({
-      dateOfCruise: this.fb.array([ this.date() ]),
-      participants: ['', Validators.required],
+      date:['',Validators.required],
+      participants: ['', [
+        Validators.required,
+        Validators.pattern("^[0-9]+$")
+      ]],
       food: [],
       contact: [],
       name: this.fb.array([ this.fullName() ]),
-      email: ['', Validators.required],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
       phone: this.fb.array([ this.number() ]),
       additional: []
     })
 
-  }
-
-  // date array function 
-  date(): FormGroup {
-    return this.fb.group({
-      month: [],
-      day: [],
-      year: [],
-      hour: [],
-      minute: []
-    })
   }
 
   // name array function
@@ -54,38 +48,19 @@ export class CruiseFormComponent implements OnInit {
     })
   }
 
-  // hour array
-  hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-  // minutes array
-  mimutes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
-  get dateOfCruise() {
-    return this.form.get('dateOfCruise');
+  // on submit method
+  onSubmit(){
+    alert("Data Send Successfully !!");
+    console.log(this.form.value);
   }
 
-  get participants() {
-    return this.form.get('participants');
-  }
-
-  get food() {
-    return this.form.get('food');
-  }
-
-  get contact() {
-    return this.form.get('contact');
-  }
-
-  get name() {
-    return this.form.get('name');
-  }
-
-  get email() {
-    return this.form.get('email');
-  }
-
-  get phone() {
-    return this.form.get('phone');
-  }
+  // get controls
+  get date() {return this.form.get('date');}
+  get participants() {return this.form.get('participants');}
+  get food() {return this.form.get('food');}
+  get contact() {return this.form.get('contact');}
+  get name() {return this.form.get('name');}
+  get email() {return this.form.get('email');}
+  get phone() {return this.form.get('phone');}
 
 }
