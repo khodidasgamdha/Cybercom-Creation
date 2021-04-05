86a657be217763272fa83947399297313b62ea5f
Khodidas Gamdha <kagamdha@gmail.com>
2021-04-05 18:56:37 +0530

    Basic Practice

--- Practice/src/App.vue
+++ Practice/src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <h1>Hello, from Vue Js</h1>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+export default {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: rgb(66, 66, 66);
+  color: aliceblue;
 }
 </style>
