a02e5ffb1dd6564cfd979f00c89f6feace0a602e
Khodidas Gamdha <kagamdha@gmail.com>
2021-04-13 19:13:05 +0530

    ref, directive, mixins, params, http

--- practice/src/components/Home.vue
+++ practice/src/components/Home.vue
@@ -1,49 +1,48 @@
 <template>
-<div>
-    <hr>
-    <!-- props -->
-    <h1 v-bind="$attrs">Hello, {{name}} : Like {{like}}</h1>
-    <hr>
-
-    <!-- Dynamic Components -->
     <div>
-        <button @click="activeTab = 'TabA'">Tab A</button>
-        <button @click="activeTab = 'TabB'">Tab B</button>
-        <button @click="activeTab = 'TabC'">Tab C</button>
+        <hr />
+        <!-- props -->
+        <h1 v-bind="$attrs">Hello, {{ name }} : Like {{ like }}</h1>
+        <hr />
+
+        <!-- Dynamic Components -->
+        <div>
+            <button @click="activeTab = 'TabA'">Tab A</button>
+            <button @click="activeTab = 'TabB'">Tab B</button>
+            <button @click="activeTab = 'TabC'">Tab C</button>
+        </div>
+        <component :is="activeTab" />
+        <hr />
     </div>
-    <component :is="activeTab" />
-    <hr>
-</div>
 </template>
 
 <script>
-import TabA from './TabA'
-import TabB from './TabB'
-import TabC from './TabC'
+import TabA from "./TabA";
+import TabB from "./TabB";
+import TabC from "./TabC";
 
 export default {
-    name: 'Home',
+    name: "Home",
     data() {
         return {
-            activeTab: 'TabA'
-        }
+            activeTab: "TabA",
+        };
     },
     components: {
         TabA,
         TabB,
-        TabC
+        TabC,
     },
     props: {
         name: {
             type: String,
-            default: 'default Name'
+            default: "default Name",
         },
-        like: Number
+        like: Number,
     },
-    inheritAttrs: false
-}
+    inheritAttrs: false,
+};
 </script>
 
 <style>
-
 </style>
