71e3612a6c46fcc6ddf0a0af8f7d5ca4f5f5ff8a
Khodidas Gamdha <kagamdha@gmail.com>
2021-04-21 19:15:38 +0530

    Todo App with Vuex

---
+++ vuex-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <AddTodo />
+            <FilterTodos />
+            <Todos />
+        </div>
+    </div>
+</template>
+
+<script>
+import Todos from "./components/todos";
+import AddTodo from "./components/addTodo";
+import FilterTodos from "./components/filterTodos";
+
+export default {
+    name: "App",
+    components: {
+        Todos,
+        AddTodo,
+        FilterTodos,
+    },
+};
+</script>
+
+<style>
+body {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    line-height: 1.6;
+    background: #e8f7f0;
+}
+.container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+}
+</style>
