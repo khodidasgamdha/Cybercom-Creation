3a6730a531489c01afc79957a4b21d6de96eef47
Khodidas Gamdha <kagamdha@gmail.com>
2021-06-04 10:24:07 +0530

    graphQL Practice

---
+++ Practice/server/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const { mainCards, animals, categories } = require("./db");
+const typeDefs = require("./schema");
+const Query = require("./resolvers/Query");
+const Animal = require("./resolvers/Animal");
+const Category = require("./resolvers/Category");
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query,
+        Animal,
+        Category,
+    },
+    context: {
+        mainCards,
+        animals,
+        categories
+    }
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
